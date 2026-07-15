49784cd20234b508a3863ac0857361fe5c4f04f3
Sangigi <138624789+Sangigi@users.noreply.github.com>
2026-07-15 12:11:20 -0600

    Change 'Hello World' to 'Goodbye World'

--- usuarios.php
+++ usuarios.php
@@ -1121,6 +1121,12 @@ function eliminarUsuario($conn)
                         <?php endif; ?>
                         <?php if ($_SESSION['usuario_rol'] === 'admin'): ?>
                             <li class="nav-item">
+                                <a class="nav-link" href="comisiones_config.php">
+                                    <i class="fas fa-percentage"></i>
+                                    Comisiones
+                                </a>
+                            </li>
+                            <li class="nav-item">
                                 <a class="nav-link" href="configuracion.php">
                                     <i class="fas fa-cogs"></i>
                                     Configuración
